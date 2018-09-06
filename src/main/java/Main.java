c1974736ba86fce0a90ae25942b1e2d84e2cbd06
Marcel Berkholz <code@berkholz.org>
2018-09-07 00:21:37 +0200

    add source for loading and saving a xml configuration add some comments add import from other maven projects, e.g. Configurationframework and Helperfuntions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.io.File;
+import java.text.DecimalFormat;
 import org.apache.commons.cli.ParseException;
 import org.apache.logging.log4j.LogManager;
 import org.berkholz.pamoba.CmdLineOption;
+import org.berkholz.helperfunctions.HelperFunctions;
+import org.berkholz.configurationframework.Configuration;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -18,23 +20,38 @@ public class Main {
 
 	// Logger for this class. See, https://logging.apache.org/log4j/2.x/
 	private static final org.apache.logging.log4j.Logger LOG = LogManager.getLogger(Main.class.getName());
-	
+
 	public static void main(String[] args) {
+
+		// MEASURE START
 		long measureStartTime = System.currentTimeMillis();
 		LOG.debug("Defining start time for measuring the execution time of the complete program: " + measureStartTime);
-		
-		LOG.debug("Initiating command line options.");
+
+		// COMMAND LINE PARSING
+		LOG.debug("Initiating the command line options.");
 		CmdLineOption commandLineOptions = new CmdLineOption(args);
 		try {
 			commandLineOptions.setCmdLineOptions();
 		} catch (ParseException ex) {
 			LOG.error("Something went wrong on parsing the command line options: " + ex.getLocalizedMessage());
 		}
-		
+
+		// CONFIG SAVE
+//		Configuration.save(new MainConfiguration(), new File(HelperFunctions.getUserHomeDirectory() + File.separator + "pamoba.conf.xml"));
+
+		// CONFIG LOAD
+		MainConfiguration mainConfig = (MainConfiguration) Configuration.load(MainConfiguration.class, new File(HelperFunctions.getUserHomeDirectory() + File.separator + "pamoba.conf.xml"));
+
+		// PRINT CONFIG
+		System.out.println("Config: " + mainConfig.toString());
+
+		// MEASURE END
 		long measureEndTime = System.currentTimeMillis();
 		LOG.debug("Defining end time for measuring the execution time of the complete program: " + measureEndTime);
-		
-		LOG.info(String.format("Backup execution time in seconds: %d", measureEndTime - measureStartTime));
-		
+
+		// LOG
+		LOG.info(String.format("Execution time: %s %s",
+				new DecimalFormat("#0.0000").format((measureEndTime - measureStartTime) / 1000d), "seconds"));
+
 	}
 }
