6145485fcff919ad41fecdc201dcb06a6346b37a
Marcel Berkholz <code@berkholz.org>
2018-09-07 01:09:57 +0200

    add Action Items change case of variables to uppercase override toString() for printing the configuration

--- src/main/java/org/berkholz/pamoba/config/DatabaseSelectConfiguration.java
+++ src/main/java/org/berkholz/pamoba/config/DatabaseSelectConfiguration.java
@@ -18,37 +18,52 @@ class DatabaseSelectConfiguration {
 	 */
 	// SELECT <COLUMN>
 	@XmlElement(name = "DatabaseSelectTableColumn", required = true)
-	private final String databaseTableColumn;
+	private final String DATABASE_TABLE_COLUMN;
 
 	// FROM <TABLE>
 	@XmlElement(name = "DatabaseSelectTable", required = true)
-	private final String databaseTableName;
+	private final String DATABASE_TABLE_NAME;
 
 	// WHERE <CONDITION>
 	@XmlElement(name = "DatabaseSelectCondition", nillable = true)
-	private final String databaseSelectCondition;
+	private final String DATABASE_SELECT_CONDITION;
 
 	/**
 	 * CONSTRCUTOR
 	 */
+	// TODO: add javadoc
 	public DatabaseSelectConfiguration() {
-		this.databaseSelectCondition = null;
-		this.databaseTableColumn = "id";
-		this.databaseTableName = "mdl_course";
+		DATABASE_SELECT_CONDITION = null;
+		DATABASE_TABLE_COLUMN = "id";
+		DATABASE_TABLE_NAME = "mdl_course";
+	}
+
+	/**
+	 * METHODS
+	 */
+	// TODO: add javadoc
+	public String toString() {
+		return String.format(
+				"\n\t\tDATABASE_SELECT_CONDITION:\t%s\n"
+				+ "\t\tDATABASE_TABLE_COLUMN:\t\t%s\n"
+				+ "\t\tDATABASE_TABLE_NAME:\t\t%s\n", DATABASE_SELECT_CONDITION == null ? "-" : DATABASE_SELECT_CONDITION, DATABASE_TABLE_COLUMN, DATABASE_TABLE_NAME);
 	}
 
 	/**
 	 * GETTER AND SETTER
 	 */
-	public String getDatabaseTableColumn() {
-		return databaseTableColumn;
+	// TODO: add javadoc
+	public String getDATABASE_TABLE_COLUMN() {
+		return DATABASE_TABLE_COLUMN;
 	}
 
-	public String getDatabaseTableName() {
-		return databaseTableName;
+	// TODO: add javadoc
+	public String getDATABASE_TABLE_NAME() {
+		return DATABASE_TABLE_NAME;
 	}
 
-	public String getDatabaseSelectCondition() {
-		return databaseSelectCondition;
+	// TODO: add javadoc
+	public String getDATABASE_SELECT_CONDITION() {
+		return DATABASE_SELECT_CONDITION;
 	}
 }
