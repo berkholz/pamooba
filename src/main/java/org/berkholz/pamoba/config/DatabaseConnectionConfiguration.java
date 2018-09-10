86308dc1d1bcf2c3b4a236f648dfcc803816987b
Marcel Berkholz <code@berkholz.org>
2018-09-10 23:05:09 +0200

    add hostname variable

--- src/main/java/org/berkholz/pamoba/config/DatabaseConnectionConfiguration.java
+++ src/main/java/org/berkholz/pamoba/config/DatabaseConnectionConfiguration.java
@@ -19,11 +19,8 @@ public class DatabaseConnectionConfiguration {
 	@XmlElement(name = "DatabaseName", required = true)
 	private final String DATABASE_NAME;
 
-	@XmlElement(name = "DatabaseUsername", required = true)
-	private final String DATABASE_USERNAME;
-
-	@XmlElement(name = "DatabasePassword", required = true)
-	private final String DATABASE_PASSWORD;
+	@XmlElement(name = "DatabaseHostname", required = true)
+	private final String DATABASE_HOSTNAME;
 
 	@XmlElement(name = "DatabasePort", required = false)
 	private final int DATABASE_PORT;
@@ -32,16 +29,23 @@ public class DatabaseConnectionConfiguration {
 	@XmlElement(name = "DatabaseType", required = true)
 	private final String DATABASE_TYPE;
 
+	@XmlElement(name = "DatabaseUsername", required = true)
+	private final String DATABASE_USERNAME;
+
+	@XmlElement(name = "DatabasePassword", required = true)
+	private final String DATABASE_PASSWORD;
+
 	/**
 	 * CONSTRUCTORS
 	 */
 	// TODO: add javadoc
 	public DatabaseConnectionConfiguration() {
 		DATABASE_NAME = "Mdl_course";
+		DATABASE_PORT = 5432;
+		DATABASE_HOSTNAME = "localhost";
+		DATABASE_TYPE = "postgres,mysql";
 		DATABASE_USERNAME = "moodle";
 		DATABASE_PASSWORD = "secret";
-		DATABASE_PORT = 5432;
-		DATABASE_TYPE = "postgres,mysql";
 	}
 
 	/**
@@ -51,10 +55,11 @@ public class DatabaseConnectionConfiguration {
 	public String toString() {
 		return String.format(
 				"\n\t\tDATABASE_NAME:\t\t\t%s\n"
-				+ "\t\tDATABASE_USERNAME:\t\t%s\n"
-				+ "\t\tDATABASE_PASSWORD:\t\t***\n"
+				+ "\t\tDATABASE_HOSTNAME:\t\t%s\n"
 				+ "\t\tDATABASE_PORT:\t\t\t%s\n"
-				+ "\t\tDATABASE_TYPE:\t\t\t%s", DATABASE_NAME, DATABASE_USERNAME, DATABASE_PORT, DATABASE_TYPE);
+				+ "\t\tDATABASE_TYPE:\t\t\t%s\n"
+				+ "\t\tDATABASE_USERNAME:\t\t%s\n"
+				+ "\t\tDATABASE_PASSWORD:\t\t***\n", DATABASE_NAME, DATABASE_HOSTNAME, DATABASE_PORT, DATABASE_TYPE, DATABASE_USERNAME);
 	}
 
 	/**
@@ -66,6 +71,21 @@ public class DatabaseConnectionConfiguration {
 	}
 
 	// TODO: add javadoc
+	public String getDATABASE_HOSTNAME() {
+		return DATABASE_HOSTNAME;
+	}
+
+	// TODO: add javadoc
+	public int getDATABASE_PORT() {
+		return DATABASE_PORT;
+	}
+
+	// TODO: add javadoc
+	public String getDATABASE_TYPE() {
+		return DATABASE_TYPE;
+	}
+
+	// TODO: add javadoc
 	public String getDATABASE_USERNAME() {
 		return DATABASE_USERNAME;
 	}
@@ -74,10 +94,4 @@ public class DatabaseConnectionConfiguration {
 	public String getDATABASE_PASSWORD() {
 		return DATABASE_PASSWORD;
 	}
-
-	// TODO: add javadoc
-	public int getDATABASE_PORT() {
-		return DATABASE_PORT;
-	}
-
 }
