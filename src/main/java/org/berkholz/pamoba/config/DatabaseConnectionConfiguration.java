9579760a6716a60f919a5491af33ebdb74a25d54
Marcel Berkholz <code@berkholz.org>
2018-09-14 01:18:02 +0200

    add variable for enabling or disabling verify of ssl encrypted connections add java doc change default database setting in constructor to mysql

--- src/main/java/org/berkholz/pamoba/config/DatabaseConnectionConfiguration.java
+++ src/main/java/org/berkholz/pamoba/config/DatabaseConnectionConfiguration.java
@@ -35,23 +35,36 @@ public class DatabaseConnectionConfiguration {
 	@XmlElement(name = "DatabasePassword", required = true)
 	private final String DATABASE_PASSWORD;
 
+	@XmlElement(name = "DatabaseSSLenabled")
+	private final Boolean DATABASE_SSL_ENABLED;
+
 	/**
 	 * CONSTRUCTORS
 	 */
-	// TODO: add javadoc
+	/**
+	 * Constructor to initialize the database connection with a default
+	 * configuration.
+	 */
 	public DatabaseConnectionConfiguration() {
 		DATABASE_NAME = "Mdl_course";
-		DATABASE_PORT = 5432;
+		DATABASE_PORT = 3306;
 		DATABASE_HOSTNAME = "localhost";
-		DATABASE_TYPE = "postgres,mysql";
+		DATABASE_TYPE = "mysql";
 		DATABASE_USERNAME = "moodle";
 		DATABASE_PASSWORD = "secret";
+		DATABASE_SSL_ENABLED = true;
 	}
 
 	/**
-	 * METHODS
+	 * *********
+	 * METHODS * *********
 	 */
-	// TODO: add javadoc
+	/**
+	 * Override toString() method to get all variables printed out.
+	 *
+	 * @return All variables as formatted String.
+	 */
+	@Override
 	public String toString() {
 		return String.format(
 				"\n\t\tDATABASE_NAME:\t\t\t%s\n"
@@ -59,39 +72,74 @@ public class DatabaseConnectionConfiguration {
 				+ "\t\tDATABASE_PORT:\t\t\t%s\n"
 				+ "\t\tDATABASE_TYPE:\t\t\t%s\n"
 				+ "\t\tDATABASE_USERNAME:\t\t%s\n"
-				+ "\t\tDATABASE_PASSWORD:\t\t***\n", DATABASE_NAME, DATABASE_HOSTNAME, DATABASE_PORT, DATABASE_TYPE, DATABASE_USERNAME);
+				+ "\t\tDATABASE_PASSWORD:\t\t***\n"
+				+ "\t\tDATABASE_SSL_ENABLED:\t\t***\n", DATABASE_NAME, DATABASE_HOSTNAME, DATABASE_PORT, DATABASE_TYPE, DATABASE_USERNAME, DATABASE_SSL_ENABLED);
 	}
 
 	/**
 	 * GETTER AND SETTER
 	 */
-	// TODO: add javadoc
+	/**
+	 * Return the database name to query against.
+	 *
+	 * @return Return the database name to query against.
+	 */
 	public String getDATABASE_NAME() {
 		return DATABASE_NAME;
 	}
 
-	// TODO: add javadoc
+	/**
+	 * Return the hostname where the database is hosted.
+	 *
+	 * @return Return the hostname where the database is hosted.
+	 */
 	public String getDATABASE_HOSTNAME() {
 		return DATABASE_HOSTNAME;
 	}
 
 	// TODO: add javadoc
+	/**
+	 *
+	 * @return
+	 */
 	public int getDATABASE_PORT() {
 		return DATABASE_PORT;
 	}
 
 	// TODO: add javadoc
+	/**
+	 *
+	 * @return
+	 */
 	public String getDATABASE_TYPE() {
 		return DATABASE_TYPE;
 	}
 
 	// TODO: add javadoc
+	/**
+	 *
+	 * @return
+	 */
 	public String getDATABASE_USERNAME() {
 		return DATABASE_USERNAME;
 	}
 
 	// TODO: add javadoc
+	/**
+	 *
+	 * @return
+	 */
 	public String getDATABASE_PASSWORD() {
 		return DATABASE_PASSWORD;
 	}
+
+	// TODO: add javadoc
+	/**
+	 *
+	 * @return
+	 */
+	public Boolean getDATABASE_SSL_ENABLED() {
+		return DATABASE_SSL_ENABLED;
+	}
+
 }
